ddb3557499d91dd902811a42435071721516292d
Gray-William <gray_william@bah.com>
2023-01-11 11:16:46 -0500

    first commit

---
+++ subgraph-reviews/resolvers.js
@@ -0,0 +1,31 @@
+
+// (parent, args, context, info)
+
+const resolvers = {
+    Query: {
+        latestReviews: (_, __, { dataSources }) => {
+            return dataSources.reviewsAPI.getLatestReviews();
+        },
+    },
+    Mutation: {
+        submitReview: (_, { locationReview }, { dataSources }) => {
+            const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
+            return { code: 200, success: true, message: 'success', locationReview: newReview };
+        }
+    },
+    Review: {
+        location: ({ locationId }) => {
+            return { id: locationId }
+        }
+    },
+    Location: {
+        overallRating: ({ id }, _, { dataSources }) => {
+            return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+        },
+        reviewsForLocation: ({ id }, _, { dataSources }) => {
+            return dataSources.reviewsAPI.getReviewsForLocation(id)
+        }
+    }
+};
+
+module.exports = resolvers;
